83af50061bfb05b13a72a7c8d07cf884e354a05b
tundexijim <freetunde2008@yahoo.com>
2023-09-02 01:37:08 +0300

    vercel

--- src/utils/getallProducts.js
+++ src/utils/getallProducts.js
@@ -1,5 +1,5 @@
 export const getallProducts = async () =>{
-    const res = await fetch(`${process.env.BASE_URL}/api/products`, {
+    const res = await fetch(`${process.env.VERCEL_URL}/api/products`, {
         cache: "no-store",
       });
     
